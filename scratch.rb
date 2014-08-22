820a1a663a3283a29d5e0539a0ee5a6f9e743b56
Shawn Veader <shawn@veader.org>
2014-08-21 23:53:16 -0400

    WIP on the scratch file

--- scratch.rb
+++ scratch.rb
@@ -8,41 +8,47 @@ require "pp"
 
 Dotenv.load
 
-mmr_client = \
+@mmr_client = \
   Mmf::Client.new do |config|
     config.client_key    = ENV["MMR_CLIENT_KEY"]
     config.client_secret = ENV["MMR_CLIENT_SECRET"]
     config.access_token  = ENV["MMR_ACCESS_TOKEN"]
   end
 
-# puts "-"*80
-# json_me = mmr_client.me
-# pp json_me
-# puts "-"*80
-# json_workouts = mmr_client.workouts(user: json_me["id"])
-# pp json_workouts
-workout = mmr_client.workout(workout_id: 688203659, field_set: "time_series")
-pp workout
-puts "-"*80
+# returns JSON user object
+def get_current_user
+  @mmr_client.me
+end
 
+# returns collection of JSON workout objects
+def get_workouts_for_user(user_id, options={})
+  final_options = options.merge(user: user_id)
+  @mmr_client.workouts(final_options)
+end
 
-# TODO: change creator
-gpx_namespace_bs = {
-  "version"             => "1.1",
-  "creator"             => "MMR_Workout_Converter",
-  "xmlns"               => "http://www.topografix.com/GPX/1/1",
-  "xmlns:xsi"           => "http://www.w3.org/2001/XMLSchema-instance",
-  "xsi:schemaLocation"  => "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd",
-  "xmlns:gpxtpx"        => "http://www8.garmin.com/xmlschemas/GpxExtensionsv3.xsd",
-}
+# returns JSON workout object
+def get_workout(workout_id)
+  @mmr_client.workout(workout_id: workout_id, field_set: "time_series")
+end
 
-# 2014-08-13T11:48:45Z
-time_format = "%Y-%m-%dT%H:%M:%SZ"
-start_time = DateTime.parse(workout["start_datetime"])
-time_series = workout["time_series"]
-time_series = [time_series] unless time_series.is_a?(Array)
+# returns Nokogiri builder
+def create_gpx_for_workout(workout)
+  # TODO: change creator
+  gpx_namespace_bs = {
+    "version"             => "1.1",
+    "creator"             => "MMR_Workout_Converter",
+    "xmlns"               => "http://www.topografix.com/GPX/1/1",
+    "xmlns:xsi"           => "http://www.w3.org/2001/XMLSchema-instance",
+    "xsi:schemaLocation"  => "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd",
+    "xmlns:gpxtpx"        => "http://www8.garmin.com/xmlschemas/GpxExtensionsv3.xsd",
+  }
 
-builder = \
+  # example: 2014-08-13T11:48:45Z
+  time_format = "%Y-%m-%dT%H:%M:%SZ"
+  start_time = DateTime.parse(workout["start_datetime"])
+  time_series = workout["time_series"]
+  time_series = [time_series] unless time_series.is_a?(Array)
+
   Nokogiri::XML::Builder.new do |xml|
     xml.gpx(gpx_namespace_bs) do
       xml.metadata {
@@ -71,8 +77,14 @@ builder = \
       }
     end
   end
-
-File.open("./tmp/generated.xml", "w+") do |f|
-  f << builder.to_xml
 end
-# puts builder.to_xml
+
+# -------------------------------------------------------------------------
+json_me = get_current_user
+json_workouts = get_workouts_for_user(json_me["id"], started_after: "2014-08-01T00:00:00Z")
+pp json_workouts
+
+# gpx = create_gpx_for_workout(get_workout(688203659))
+# File.open("./tmp/generated.xml", "w+") do |f|
+#   f << gpx.to_xml
+# end
