9c4b72c9df9a25e98967f29ec8e0daace182ed4a
Shawn Veader <shawn@veader.org>
2014-08-27 00:34:03 -0400

    forgot these constants too

--- lib/models/mmr_client.rb
+++ lib/models/mmr_client.rb
@@ -7,8 +7,8 @@ module MMR
     def initialize(auth_token)
       self.client_instance = \
         Mmf::Client.new do |config|
-          config.client_key    = ENV["MMR_CLIENT_KEY"]
-          config.client_secret = ENV["MMR_CLIENT_SECRET"]
+          config.client_key    = ENV["MMF_API_KEY"]
+          config.client_secret = ENV["MMF_API_SECRET"]
           config.access_token  = auth_token
         end
     end
