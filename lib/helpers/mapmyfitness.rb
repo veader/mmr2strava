fa22d6f7dea2174d11d337a08160f53d9cea433d
Shawn Veader <shawn@veader.org>
2014-08-27 01:00:42 -0400

    trying to see the oauth response from MMF

--- lib/helpers/mapmyfitness.rb
+++ lib/helpers/mapmyfitness.rb
@@ -2,6 +2,7 @@ module MMRToStrava
   module MapMyFitness
     def handle_mapmyfitness_oauth
       auth_hash = request.env["omniauth.auth"]
+      puts "MMF Auth Hash: #{auth_hash}"
       current_user.mmr_user_id = auth_hash["uid"]
       current_user.mmr_token = auth_hash["credentials"]["token"]
       current_user.save
